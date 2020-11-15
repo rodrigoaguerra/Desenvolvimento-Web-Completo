3b5c71996942762bb52e39e2fb6fdbf929ab3880
Rodrigo Alves Guerra <rodrigoa.guerra@hotmail.com>
2020-11-15 15:30:33 -0300

    My firts commit

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController {
+  public function index() {
+    echo 'Chegamos ao InIndexController e disparamentos a action index';
+  }
+
+  public function sobreNos() {
+    echo 'Chegamos ao InIndexController e disparamentos a action Sobre nos';
+  }
+}
+
+?>
