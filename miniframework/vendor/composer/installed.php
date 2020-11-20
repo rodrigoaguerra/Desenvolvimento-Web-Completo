15ba5e4a7a7a59d39632ed81092544182dc748ac
Rodrigo Alves Guerra <rodrigoa.guerra@hotmail.com>
2020-11-19 21:01:32 -0300

    Add composer

---
+++ miniframework/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '11658650d3e89b30702b95855870d306172d857a',
+    'name' => 'project/app',
+  ),
+  'versions' => 
+  array (
+    'project/app' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '11658650d3e89b30702b95855870d306172d857a',
+    ),
+  ),
+);
