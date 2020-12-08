39491eef02a4cb9fecfc5456229cfc5197494aec
rodrigoa.guerra@hotmail.com <rodrigoa.guerra@hotmail.com>
2020-12-08 06:27:19 -0300

    Init app lista de tarefa!

---
+++ lista_tarefas/app/tarefa_controller.php
@@ -0,0 +1,15 @@
+<?php
+  require_once "../app/tarefa.model.php";
+  require_once "../app/tarefa.service.php";
+  require_once "../app/conexao.php";
+
+  $tarefa = new Tarefa();
+  $tarefa->__set('tarefa', $_POST['tarefa']);
+
+  $conexao = new Conexao();
+
+  $tarefaService = new TarefaServices($conexao, $tarefa);
+  $tarefaService->inserir();
+
+  header('Location: nova_tarefa.php?inclusao=1');
+?>
---
+++ lista_tarefas/public/tarefa_controller.php
@@ -0,0 +1,3 @@
+<?php
+  require_once '../app/tarefa_controller.php';
+?>
